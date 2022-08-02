3c384d88b8562bc3bd5c217c87a18ba01de6bf86
Marc Dwyer <marcdwyer1738@gmail.com>
2022-08-01 23:59:29 -0500

    spiral nums;

--- spiralNums.ts
+++ spiralNums.ts
@@ -8,11 +8,10 @@ function spiralOrder(matrix: number[][]): number[] {
   if (!matrix.length) return nums;
 
   let top = 0;
-
   let left = 0;
-
   let right = matrix[0].length - 1;
   let bottom = matrix.length - 1;
+
   const size = matrix.length * matrix[0].length;
 
   const checkSize = () => nums.length < size;
@@ -22,33 +21,30 @@ function spiralOrder(matrix: number[][]): number[] {
       nums.push(matrix[top][i]);
     }
     top++;
-
     for (let i = top; i <= bottom && checkSize(); i++) {
       nums.push(matrix[i][right]);
     }
     right--;
-
-    for (let i = right; i >= left && checkSize(); i--) {
+    for (let i = right; i >= 0 && checkSize(); i--) {
       nums.push(matrix[bottom][i]);
     }
     bottom--;
-
-    for (let i = bottom; i >= top && checkSize(); i++) {
+    for (let i = bottom; i >= top && checkSize(); i--) {
       nums.push(matrix[i][left]);
     }
-
     left++;
   }
+
   return nums;
 }
 
-console.log(
-  spiralOrder([
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-  ])
-);
+// console.log(
+//   spiralOrder([
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//   ])
+// );
 
 console.log(
   spiralOrder([
@@ -57,3 +53,12 @@ console.log(
     [7, 8, 9],
   ])
 );
+
+// console.log(
+//   spiralOrder([
+//     [1, 2, 3, 5],
+//     [4, 5, 6, 5],
+//     [7, 8, 9, 7],
+//     [7, 8, 9, 7],
+//   ])
+// );
