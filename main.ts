cc51dc1edab38a4705c876c4db943e4351a62488
Marc Dwyer <marcdwyer1738@gmail.com>
2022-08-01 21:50:50 -0500

    more leetcodes

--- main.ts
+++
@@ -1,8 +0,0 @@
-const set = new Set();
-set.add(1).add(2).add(3);
-
-for (const n of set.values()) {
-  console.log({ n });
-}
-
-new Set().size;
---
+++ spiralNums.ts
@@ -0,0 +1,59 @@
+/**
+ * Use several pointers
+ */
+
+function spiralOrder(matrix: number[][]): number[] {
+  const nums: number[] = [];
+
+  if (!matrix.length) return nums;
+
+  let top = 0;
+
+  let left = 0;
+
+  let right = matrix[0].length - 1;
+  let bottom = matrix.length - 1;
+  const size = matrix.length * matrix[0].length;
+
+  const checkSize = () => nums.length < size;
+
+  while (checkSize()) {
+    for (let i = left; i <= right && checkSize(); i++) {
+      nums.push(matrix[top][i]);
+    }
+    top++;
+
+    for (let i = top; i <= bottom && checkSize(); i++) {
+      nums.push(matrix[i][right]);
+    }
+    right--;
+
+    for (let i = right; i >= left && checkSize(); i--) {
+      nums.push(matrix[bottom][i]);
+    }
+    bottom--;
+
+    for (let i = bottom; i >= top && checkSize(); i++) {
+      nums.push(matrix[i][left]);
+    }
+
+    left++;
+  }
+  return nums;
+}
+
+console.log(
+  spiralOrder([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ])
+);
+
+console.log(
+  spiralOrder([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ])
+);
